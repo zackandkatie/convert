0d981cf37a3d67fe5f9ce9374cba4cff47050e00
Zack <zackandkatievii@gmail.com>
2023-11-24 15:38:33 +0800

    first commit

---
+++ R/convert_xpt_to_sas.R
@@ -0,0 +1,41 @@
+#' convert xpt to sas function
+#'
+#' @param xpt_folder input a file directory where your xpt files is
+#'
+#' @return sas7bdat files
+#' @export 
+#'
+#' @examples
+#' xpt_folder <- '/Users/zack/convert_xpt_to_sas'
+#' xpt_files <- list.files(path = xpt_folder, pattern = "\\.xpt$", full.names = TRUE)
+#' for (file in xpt_files) {
+#' sas_file <- gsub("\\.xpt$", ".sas7bdat", file)
+#' if (!file.exists(sas_file)) {
+#'   df <- haven::read_xpt(file)
+#'   haven::write_sas(df, sas_file)
+#'   cat("Converted", file, "to SAS7BDAT format.\n")
+#' } else {
+#'   cat(sas_file, "already exists. Skipping...\n")
+#'   }
+#'   }
+convert_xpt_to_sas <- function(xpt_folder){
+  # Load the necessary package
+  library(haven)
+  
+  
+  
+  # List all the XPT files in the folder
+  xpt_files <- list.files(path = xpt_folder, pattern = "\\.xpt$", full.names = TRUE)
+  
+  # Loop over each XPT file and convert it to SAS7BDAT format
+  for (file in xpt_files) {
+    sas_file <- gsub("\\.xpt$", ".sas7bdat", file)
+    if (!file.exists(sas_file)) {
+      df <- haven::read_xpt(file)
+      haven::write_sas(df, sas_file)
+      cat("Converted", file, "to SAS7BDAT format.\n")
+    } else {
+      cat(sas_file, "already exists. Skipping...\n")
+    }
+  }
+}
